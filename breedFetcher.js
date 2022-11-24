23e1d011f4193fec7e00185c9a8f4f48cfb1d66f
t-iffany <tiffanyxleong@gmail.com>
2022-11-24 21:22:41 +0000

    Added if statements to check for edge cases. Prints out description depending on breed you pass it.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,33 @@
-// Write the logic to fetch data from teh API endpoint using request
+// Write the logic to fetch data from the API endpoint using request
+
+// command line arguments
+const args = process.argv[2];
+let breed = args;
+// node type in Breed and it will take us to the URL
+// node breedFetcher.js breed
+let url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 // use request to make HTTP request for HTML of the website homepage
 const request = require('request');
-// print out the body content to the terminal
-request('https://api.thecatapi.com/v1/breeds/search?q=siberian', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //  console.log('body:', body); // Print the HTML for the website homepage. Body is a string
-  const data = JSON.parse(body);      // use json.parse to convert body into an object
-//  console.log('first entry: weight ', data[0].weight);      // access the first entry in the data array and print out the description
-});
+
+const breedFetcher = (url) => {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('error: REQUEST FAILED', error);  // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      return;    // so it stops trying to keep runnig to the next condition
+    }
+
+    const data = JSON.parse(body);      // use json.parse to convert body into an object
+    
+    if (data.length === 0) {
+      console.log("Breed not found");
+      return;
+    }
+    // print out description
+    console.log('Description:', data[0].description); // data/body > array > object ; data[0] is index of array ; within the array, we want the object key description
+  });
+};
+
+//run app breedFetcher
+breedFetcher(url);
